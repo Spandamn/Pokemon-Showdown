805fc52aec2d5f16ef4e30ce271596631d878d30
Insist <DeathlyPlays@users.noreply.github.com>
2017-07-16 08:57:52 -0500

    Fix eslint issue

--- chat.js
+++ chat.js
@@ -918,7 +918,7 @@ Chat.loadCommands = function () {
 	for (let file of fs.readdirSync(path.resolve(__dirname, 'chat-plugins'))) {
 		if (file.substr(-3) !== '.js' || file === 'info.js' || file === 'github.js') continue;
 		Object.assign(commands, require('./chat-plugins/' + file).commands);
-	}	
+	}
 
 	try {
 		Object.assign(commands, require('./chat-plugins/github').commands);

4de8bb4595acd0067eae9b10fb091466fa0456ad
Spandamn <spandan.punwatkar@gmail.com>
2017-09-03 19:36:42 +0530

    Update moves.js

--- mods/seasonal/statuses.js
+++ mods/seasonal/statuses.js
@@ -62,7 +62,7 @@ exports.BattleStatuses = {
 	},
 	innovamania: {
 		exists: true,
-		onStart: function () {
+		onStart: function (target, source) {
 			let sentences = ['Don\'t take this seriously', 'These Black Glasses sure look cool', 'Ready for some fun?( ͡° ͜ʖ ͡°)', '( ͡° ͜ʖ ͡°'];
 			this.add('c|@innovamania|' + sentences[this.random(4)]);
 			this.boost({atk: 6, def: 6, spa: 6, spd: 6, spe: 6, accuracy: 6}, source);

48c624575d34ed6ae921d16f13eac23d20e08561
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2020-08-18 05:38:58 +0530

    Apply suggestions from code review
    Co-authored-by: Instruct <66930476+xInstruct@users.noreply.github.com>

--- data/mods/ssb/random-teams.ts
+++ data/mods/ssb/random-teams.ts
@@ -141,7 +141,7 @@ export const ssbSets: SSBSets = {
 		species: 'Dragonite', ability: 'Speed Control', item: 'Metal Coat', gender: '',
 		moves: ['Swords Dance', 'Thousand Arrows', 'Double Iron Bash'],
 		signatureMove: 'Pandora\'s Box',
-		evs: {atk: 252, def: 4, spe: 252}, nature: 'Jolly',
+		evs: {atk: 252, def: 4, spe: 252}, nature: 'Jolly', shiny: 2,
 	},
 	Celine: {
 		species: 'Lucario', ability: 'Guardian Armor', item: 'Leftovers', gender: 'F',
@@ -649,7 +649,6 @@ export class RandomStaffBrosTeams extends RandomTeams {
 			set.moves.push(ssbSet.signatureMove);
 
 			// Any set specific tweaks occur here.
-			if (set.name === 'Celestial' && this.randomChance(1, 2)) set.shiny = true;
 			if (set.name === 'quadrophenic') set.moves[this.random(2) + 1] = 'Conversion';
 			if (set.name === 'aegii' && this.randomChance(1, 2)) {
 				set.moves[set.moves.indexOf('Shadow Claw')] = 'Shadow Ball';
